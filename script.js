c2d26483e86dbff985ae1c23d2e9815c3143e7da
meaux21 <maurice.grimes21@gmail.com>
2022-09-13 11:58:11 -0400

    Events Demo

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const reset = () => {
+    console.log('Clicked!!!!!')
+}
+const randNum = () => {
+    console.log(Math.floor(Math.random() * 255))
+}
+// dom property way
+const resetBtn = document.getElementById('reset-btn')
+resetBtn.onclick = reset
+
+// add event listener 
+const btn = document.querySelector('#reset-btn2')
+btn.addEventListener('click',reset)
+btn.addEventListener('click', randNum)
+btn.addEventListener('click', function(){
+    console.log('third function')
+})
